ebf1609a00753cb02ba26a7aa00b50e1885ce7d6
Yimin <765495715@qq.com>
2026-09-18 16:04:36 +0800

    手部接口修正

---
+++ source/g1_wuji_doorman/g1_wuji_doorman/tasks/manager_based/g1_wuji_doorman/mdp/actions.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Custom action terms for the G1+Wuji DoorMan task."""
